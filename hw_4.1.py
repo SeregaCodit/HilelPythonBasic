dfa4b3bd3a825cec6485b5c5961e1084741f7b7d
DobriyKot <serega.codit@gmail.com>
2024-06-01 17:27:03 +0300

    add corrections in hw_4.1

--- hw_4.1.py
+++ hw_4.1.py
@@ -10,17 +10,13 @@ import random
 max_list_length = 10
 
 # створюємо не пустий список
-lst = [random.randint(0, 50) for i in range(random.randint(1, max_list_length))]
+lst = [random.randint(0, 10) for i in range(random.randint(1, max_list_length))]
 last_index_in_list = len(lst) - 1
 
 # забезпечуємо, що у списку будуть нулі
 if 0 not in lst:
-    # забезпечимо, що завжди бутуть присутні числа більше 0
-    iterations = random.randint(1, last_index_in_list - 1)
-    for i in range(iterations):
-        index = random.randint(0, last_index_in_list)
-        if lst[index]:
-            lst[index] = 0
+    index = random.randint(0, last_index_in_list)
+    lst[index] = 0
 
 print("original list", lst, sep=": ", end=" => ")
 
