7d23061f120612c962ff24f69b6fbcbcb815a592
DobriyKot <serega.codit@gmail.com>
2024-06-01 17:12:28 +0300

    add corrections in hw_4.1

--- hw_4.1.py
+++ hw_4.1.py
@@ -14,6 +14,7 @@ lst = [random.randint(0, 50) for i in range(random.randint(1, max_list_length))]
 last_index_in_list = len(lst) - 1
 # забезпечуємо, що у списку будуть нулі
 if 0 not in lst:
+    # забезпечимо, що завжди бутуть присутні числа більше 0
     iterations = random.randint(1, int(len(lst) / random.randint(1, last_index_in_list)))
     for i in range(iterations):
         index = random.randint(0, last_index_in_list)
