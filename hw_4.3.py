96d2baa3007812e3608a8bbbefdb4549a2a68eee
DobriyKot <serega.codit@gmail.com>
2024-06-01 16:26:42 +0300

    add hw_4.3.py

---
+++ hw_4.3.py
@@ -0,0 +1,15 @@
+"""
+Створіть список випадкових чисел із випадковою кількістю елементів від 3 до 10.
+
+Ваше завдання - створити новий список з 3 елементів початкового списку - першим, третім і другим з кінця.
+"""
+import random
+
+
+random_list = [random.randint(0,9) for i in range(3, 10)]
+print(random_list, end=" == ")
+
+second_from_the_end = len(random_list) - 2
+target_index_list = [0, 2, second_from_the_end]
+result_list = [random_list[i] for i in target_index_list]
+print(result_list)
